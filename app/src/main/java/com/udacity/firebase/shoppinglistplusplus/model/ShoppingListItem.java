a26ca99b4394d1c644e99a2f7092f655bcfea713
Lyla <lyla@udacity.com>
2015-10-21 14:53:12 -0700

    2.13 Items - Quiz

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/model/ShoppingListItem.java
@@ -0,0 +1,10 @@
+package com.udacity.firebase.shoppinglistplusplus.model;
+
+/**
+ * Defines the data structure for ShoppingListItem objects.
+ */
+public class ShoppingListItem {
+    // TODO Create this class.
+    // Don't forget the rules for creating a POJO for Firebase.
+
+}
