0a4a4552cc7881973ac393ca47343cb4ba22905e
Lyla <lyla@udacity.com>
2015-11-06 15:10:21 -0800

    2.14 Items - Solution

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/model/ShoppingListItem.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/model/ShoppingListItem.java
@@ -4,7 +4,33 @@ package com.udacity.firebase.shoppinglistplusplus.model;
  * Defines the data structure for ShoppingListItem objects.
  */
 public class ShoppingListItem {
-    // TODO Create this class.
-    // Don't forget the rules for creating a POJO for Firebase.
+    private String itemName;
+    private String owner;
+
+    /**
+     * Required public constructor
+     */
+    public ShoppingListItem() {
+    }
+
+    /**
+     * Use this constructor to create new ShoppingListItem.
+     *
+     * @param itemName
+     */
+    public ShoppingListItem(String itemName) {
+        this.itemName = itemName;
+        /**
+         * This is a default value until we can differentiate users.
+         * Which will be soon, I promise.
+         */
+        this.owner = "Anonymous Owner";
+    }
+
+    public String getItemName() { return itemName; }
+
+    public String getOwner() {
+        return owner;
+    }
 
 }
